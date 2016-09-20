8d851a640fa59a31397f7cc1079960d6a084cb00
Sri Harsha Aditya Ravuri <shannuharsha@gmail.com>
2016-09-20 19:33:01 +0530

    Second temp change

--- about.php
+++ about.php
@@ -1,4 +1,4 @@
 <h3>Temp About</h3>
 <p> This contains the about stuff. All this data is retrieved using AJAX call into a div box</p>
-<p>So as to keep all the structure same for every page just by changint the content</p>
+<p>So as temp to keep all the structure same for every page just by changint the content</p>
 <p>Each Link has a page where it's data which is to be retrieved using AJAX is stored</p>
